8dabe9ba6f39f1e43257cb01e48da40b62727f12
Cyrene-RPG <thedarkstarchronicles@gmail.com>
2026-05-11 18:42:49 -0700

    Polish2

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Bump SW_VERSION when shipping breaking shell changes to force a refresh.
  */
 
-const SW_VERSION = 'v1.0.11';
+const SW_VERSION = 'v1.0.12';
 const SHELL_CACHE = `alysum-shell-${SW_VERSION}`;
 const ASSET_CACHE = `alysum-assets-${SW_VERSION}`;
 
@@ -25,6 +25,7 @@ const APP_SHELL = [
   '/beta-notes-library.html',
   '/reader-home.html',
   '/vault.html',
+  '/writer-resources.html',
   '/settings.html',
   '/badges.html',
   '/author-dashboard.html',
